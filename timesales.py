a0c325fc72ce16e0e5dfaa2c13e2df154508db28
bendgame <44721179+bendgame@users.noreply.github.com>
2018-11-27 13:51:59 -0600

    updating comments
    updating comments

--- timesales.py
+++ timesales.py
@@ -1,6 +1,6 @@
 #Script to download time and sales data from website https://quotes.hegnar.no/quotes/tradedump.php?
-#Example of spy https://quotes.hegnar.no/quotes/tradedump.php?date=20181121&paper=SPY.A&csv_format=csv
-#for SPY 
+#Example of complete url  https://quotes.hegnar.no/quotes/tradedump.php?date=20181121&paper=SPY.A&csv_format=csv
+#for SPY symobol
 #the script is designed to be run daily as it updates the date value in the URL to today's date. 
 
 import urllib.request as req
@@ -18,9 +18,11 @@ d = date.strftime("%d")
 
 #create URL
 url = 'https://quotes.hegnar.no/quotes/tradedump.php?date=' + y + m + d + '&paper=SPY.A&csv_format=csv'
+
+#verify url is correct
 #print (url)
 
-#open the page and close connection
+#open the page and store the data in object named http. Then close connection
 get_url = req.urlopen(url)
 http = get_url.read()
 get_url.close()
@@ -28,7 +30,9 @@ get_url.close()
 #verify the page looks correct
 #print(http)
 
-#create a file named ts + yyyymmdd and download it as csv since it is already a csv file. 
+#create a file named ts + yyyymmdd and download it as csv since the data is already csv.
 tnsfile = open('ts'+y+m+d+'.csv','wb')
 tnsfile.write(http)
+
+#Close connection to new file
 tnsfile.close()
